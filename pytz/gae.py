d528c02cb22f00d31b332488244b938a713516f6
Troy Harvey <troyharvey@gmail.com>
2017-11-29 16:55:21 -0500

    Add @jasonmyers patch from https://github.com/brstrat/Beluga/commit/1cf46ca5f435e780482413606a0f5c3f076489df

--- pytz/gae.py
+++ pytz/gae.py
@@ -2,7 +2,10 @@
     A pytz version that runs smoothly on Google App Engine.
 
     Based on http://appengine-cookbook.appspot.com/recipe/caching-pytz-helper/
-    but modified so that it can be imported normally with import pytz.
+
+    To use, add pytz to your path normally, but import it from the gae module:
+
+        from pytz.gae import pytz
 
     Applied patches:
 
@@ -17,18 +20,36 @@
         hundreds of files that we know aren't there.
 
     pytz caches loaded zoneinfos, and this module will additionally cache them
-    in App Engines's cache to avoid unzipping constantly. The cache key
-    includes the OLSON_VERSION so it is invalidated when pytz is updated.
+    in memcache to avoid unzipping constantly. The cache key includes the
+    OLSON_VERSION so it is invalidated when pytz is updated.
 """
 import os
 import logging
+import pytz
 import zipfile
 from cStringIO import StringIO
 
+# Fake memcache for when we're not running under the SDK, likely a script.
+class memcache(object):
+    @classmethod
+    def add(*args, **kwargs):
+        pass
 
-log = logging.getLogger(__name__)
+    @classmethod
+    def get(*args, **kwargs):
+        return None
+
+try:
+    # Don't use memcache outside of Google App Engine or with GAE's dev server.
+    if not os.environ.get('SERVER_SOFTWARE', '').startswith('Development'):
+        from google.appengine.api import memcache
+except ImportError:
+    pass
+
 zoneinfo = None
-zoneinfo_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'zoneinfo.zip'))
+zoneinfo_path = os.path.abspath(os.path.join(os.path.dirname(__file__),
+    'zoneinfo.zip'))
+
 
 def get_zoneinfo():
     """Cache the opened zipfile in the module."""
@@ -38,6 +59,7 @@ def get_zoneinfo():
 
     return zoneinfo
 
+
 class TimezoneLoader(object):
     """A loader that that reads timezones using ZipFile."""
     def __init__(self):
@@ -45,22 +67,18 @@ class TimezoneLoader(object):
 
     def open_resource(self, name):
         """Opens a resource from the zoneinfo subdir for reading."""
-        # Import nested here so we can run setup.py without GAE.
-        from google.appengine.api import memcache
-        from pytz import OLSON_VERSION
-
         name_parts = name.lstrip('/').split('/')
         if os.path.pardir in name_parts:
             raise ValueError('Bad path segment: %r' % os.path.pardir)
 
-        cache_key = 'pytz.zoneinfo.%s.%s' % (OLSON_VERSION, name)
+        cache_key = 'pytz.zoneinfo.%s.%s' % (pytz.OLSON_VERSION, name)
         zonedata = memcache.get(cache_key)
         if zonedata is None:
             zonedata = get_zoneinfo().read('zoneinfo/' + '/'.join(name_parts))
             memcache.add(cache_key, zonedata)
-            # log.info('Added timezone to memcache: %s' % cache_key)
+            #logging.info('Added timezone to memcache: %s' % cache_key)
         else:
-            pass # log.info('Loaded timezone from memcache: %s' % cache_key)
+            pass #logging.info('Loaded timezone from memcache: %s' % cache_key)
 
         return StringIO(zonedata)
 
@@ -74,3 +92,6 @@ class TimezoneLoader(object):
                 self.available[name] = False
 
         return self.available[name]
+
+
+pytz.loader = TimezoneLoader()
